df7c3494c45b4ef87c5389d46d3459c0ef72892f
antoniomenaescoriza <1fmenaescoriza@gmail.com>
2025-10-22 17:32:05 +0200

    jobba på app.py

--- lonframtid/application/app.py
+++ lonframtid/application/app.py
@@ -1,4 +1,38 @@
-print("hello world!") # hej från ikaros
+from flask import Flask
+app = Flask(name)
 
+
+
+
+@app.route("/")
+
+
+def startsida():
+
+
+    return "<p>Hello, World!</p>"
+
+
+
+
+@app.post("/prognos")
+
+
+def prognos_arbete():
+
+
+    return "<p>Hello, World!</p>"
+
+
+
+
+
+@app.post("/lön")
+
+
+def lön_arbete():
+
+
+    return "<p>Hello, World!</p>"
 
 
