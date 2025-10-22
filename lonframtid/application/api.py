90e64a63c1c580bf9205b2cd0b7ceabe0626cf7c
Ikaros Bernardini <ikaros.bernardini@gmail.com>
2025-10-22 17:28:46 +0200

    ändrat i api.py

--- lonframtid/application/api.py
+++ lonframtid/application/api.py
@@ -1,2 +1,5 @@
-https://skatteverket.entryscape.net/rowstore/dataset/1cad9af9-6c1e-4518-a610-c16302dd3b72 (lönestatistik per yrke och region api) 
-https://historical.api.jobtechdev.se/ad/8430129 (historiska jobb annonser api) 
+
+
+
+data = "https://skatteverket.entryscape.net/rowstore/dataset/1cad9af9-6c1e-4518-a610-c16302dd3b72" # skatteverket api
+data2 = "https://historical.api.jobtechdev.se/ad/8430129" # arbetsförmedlingens api
